c0db98b83ffb6d7723332c39c27779727f86b5da
Abdelrahman Bonna <abdelrahmanbonna@outlook.com>
2021-02-07 15:38:09 +0200

    last

--- src/index.js
+++ src/index.js
@@ -11,5 +11,16 @@ function helloworld(){
 }
 
 const img = document.createElement("img");
+const div = document.createElement("div");
+const title = document.createElement('h1');
+const par = document.createElement('p');
+const c = document.createElement('center');
+title.innerHTML = "Testing web pack"
+par.innerHTML = " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio inventore labore molestiae voluptatem possimus delectus, quos fugiat mollitia! Recusandae iusto perferendis porro? Nisi quisquam natus velit nam saepe aliquid laboriosam. "
 img.src=pic;
-document.body.appendChild(img);
+div.appendChild(img)
+c.appendChild(title);
+c.appendChild(div);
+c.appendChild(par);
+
+document.body.appendChild(c);
